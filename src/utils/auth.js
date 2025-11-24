b488fc81cb74e3f8c0bb20d865d63a6e8c2a6df2
Your Name <you@example.com>
2025-11-24 22:52:03 +0300

    first commit

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,16 +1,16 @@
 import { betterAuth } from "better-auth";
 import Database from "node:sqlite";
 import { passkey } from "better-auth/plugins/passkey";
-//import argon2 from "argon2";
+import argon2 from "argon2";
 
 export const auth = betterAuth({
 	database: new Database("sqlite.db"),
 	emailAndPassword: {
 		enabled: true,
-//		password: {
-//			hash: async (password) => argon2.hash(password, {type: argon2.argon2id, memoryCost: 65536, timeCost: 4, parallelism: 4, hashLength: 32}),
-//			verify: async ({ hash, password }) => argon2.verify(hash, password)
-//		}
+		password: {
+			hash: async (password) => argon2.hash(password, {type: argon2.argon2id, memoryCost: 65536, timeCost: 4, parallelism: 4, hashLength: 32}),
+			verify: async ({ hash, password }) => argon2.verify(hash, password)
+		}
 	},
 	plugins: [
 		passkey()
